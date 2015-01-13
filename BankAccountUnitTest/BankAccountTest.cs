c747b5cde2d63078b6823cc25ab9d3c942dfeee7
SuperStrandby <strandby@live.dk>
2015-01-13 13:41:05 +0100

    trialexam

---
+++ BankAccountUnitTest/BankAccountTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrialExamInvariantUnitTest;
+
+namespace BankAccountUnitTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void CheckOwnerOKTest()
+        {
+            var account = new BankAccount("John");
+            try
+            {
+                Assert.AreEqual("John", account.CheckOwner());
+            }
+            catch (Exception)
+            {
+                
+                Assert.Fail("Exception should not have happened");
+            }
+        }
+
+        [TestMethod]
+        public void CheckOwnerFailTest()
+        {
+            var account = new BankAccount("");
+
+            try
+            {
+                account.CheckOwner();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Owner can not be null or empty", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CheckInterestRateOK()
+        {
+            var account = new BankAccount("John", 1);
+            
+            try
+            {
+                Assert.AreEqual(1, account.CheckInterestRate());
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception should not have happened");
+            }
+        }
+
+        [TestMethod]
+        public void CheckZeroInterestRate()
+        {
+            var account = new BankAccount("John", 0);
+            try
+            {
+                Assert.AreEqual(0, account.CheckInterestRate());
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception should not have happened");
+            }
+        }
+
+        [TestMethod]
+        public void CheckInterestRateFail()
+        {
+            var account = new BankAccount("John", -1);
+
+            try
+            {
+                account.CheckInterestRate();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Interest rate must be >= 0", e.Message);
+            }
+        }
+    }
+}
---
+++ TrialExamInvariantUnitTest/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialExamInvariantUnitTest
+{
+    public class BankAccount
+    {
+        public string Owner { get; set; }
+        public double Balance { get; set; }
+        public double InterestRate { get; set; }
+
+        public BankAccount(string owner)
+        {
+            Owner = owner;
+        }
+
+        public BankAccount(string owner, double interestRate)
+        {
+            Owner = owner;
+            InterestRate = interestRate;
+        }
+
+        public BankAccount(string owner, double interestRate, double balance)
+        {
+            Owner = owner;
+            InterestRate = interestRate;
+            Balance = balance;
+            
+        }
+        public string CheckOwner()
+        {
+            if (string.IsNullOrEmpty(Owner))
+            {
+                throw new Exception("Owner can not be null or empty");
+            }
+            return Owner;  
+        }
+
+        public double CheckInterestRate()
+        {
+            if (InterestRate < 0)
+            {
+                throw new Exception("Interest rate must be >= 0");
+            }
+            return InterestRate;
+        }
+    }
+}
